45efc2dd68b7cef7fac2e03dd1655210732796f2
Balazs Kovacs <Balazs.Kovacs4@tesco.com>
2025-10-05 21:45:41 +0200

    Refactor GridComponent

--- src/app/components/grid.tsx
+++ src/app/components/grid.tsx
@@ -1,26 +1,19 @@
 "use client";
-import { useState } from "react";
 import { Grid } from "../model/grid";
 import TileComponent from "./tile";
 
-export default function GridComponent() {
-  const [grid, setGrid] = useState<Grid | undefined>(undefined);
+type GridProps = {
+  grid: Grid;
+};
 
-  const start = () => {
-    setGrid(new Grid(10, 10));
-  };
-
+const GridComponent: React.FC<GridProps> = ({ grid }) => {
   return (
     <div className="grid grid-cols-10 grid-rows-10 gap-0">
-      {grid ? (
-        grid.tiles.map((tile) => (
-          <TileComponent key={`${tile.x}-${tile.y}`} tile={tile} />
-        ))
-      ) : (
-        <button className="btn btn-blue" onClick={start}>
-          Start
-        </button>
-      )}
+      {grid.tiles.map((tile) => (
+        <TileComponent key={`${tile.x}-${tile.y}`} tile={tile} />
+      ))}
     </div>
   );
-}
+};
+
+export default GridComponent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 "use client";
-import Grid from "./components/grid";
+import { useState } from "react";
+import GridComponent from "./components/grid";
+import { Grid } from "./model/grid";
 
 export default function Home() {
+  const [grid, setGrid] = useState<Grid | undefined>(undefined);
+
+  const start = () => {
+    setGrid(new Grid(10, 10));
+  };
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>Beni Board</h1>
-        <Grid />
+        {grid ? (
+          <GridComponent grid={grid} />
+        ) : (
+          <button className="btn btn-blue" onClick={start}>
+            Start
+          </button>
+        )}
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
